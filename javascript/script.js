1f3217b8bf971b68a66ee9986d34a71de5a55650
Nina <tsikaridze@outlook.com>
2024-03-27 20:54:14 +0400

    add form validation

--- javascript/script.js
+++ javascript/script.js
@@ -7,3 +7,49 @@ let navigationForms = document.getElementById("navForms");
 burger.addEventListener("click", function () {
   navigation.classList.toggle("activeNav");
 });
+
+// Contact form
+
+let form = document.getElementById("formWrapper");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let errors = {};
+
+  let userName = document.getElementById("fname").value;
+  let userPhone = document.getElementById("phone").value;
+  let userEmail = document.getElementById("lname").value;
+  let textArea = document.getElementById("etext").value;
+
+  if (userEmail == "") {
+    errors.lname = "Email can not be empty";
+  }
+  if (userName.length < 4) {
+    errors.fname = "Username must be more than 4 characters";
+  }
+  if (userPhone == "") {
+    errors.phone = "Phone can not be empty";
+  }
+  if (textArea == "") {
+    errors.emailtext = "Message field cant be empty   ";
+  }
+  console.log(errors);
+  this.querySelectorAll(".error-text").forEach((el) => {
+    el.textContent = " ";
+  });
+
+  for (let item in errors) {
+    console.log(item);
+
+    let errorTextElement = document.getElementById("error_" + item);
+    console.log(errorTextElement);
+
+    if (errorTextElement) {
+      errorTextElement.textContent = errors[item];
+    }
+  }
+
+  if (Object.keys(errors).length === 0) {
+    this.submit();
+  }
+});
